e958b1c5afca89c637d3942e10969fd0affdde95
Michael Ryan <michaeledryan@gmail.com>
2013-04-06 23:20:09 -0400

    Starting to function

--- app.js
+++ app.js
@@ -3,15 +3,42 @@
 // ========================
 var express = require("express");
 var app = express();
+var mongoExpressAuth = require('mongo-express-auth');
+var cards = require("./cards.js")
+
+mongoExpressAuth.init({
+    mongo: { 
+        dbName: 'userAccounts',
+        collectionName: 'accounts'
+    }
+}, function(){
+    console.log('mongo ready!');
+    app.listen(3000);
+});
+
+app.use(express.bodyParser());
+app.use(express.cookieParser());
+app.use(express.session({ secret: 'M3I9H7P5GQO2PO38W667AOL6R0M998AV' }));
+
 app.get("/static/:staticFilename", function (request, response) {
   response.sendfile("static/" + request.params.staticFilename);
 });
+
+app.get("/game/:gameName", function (request, response) {
+  if (games[request.params.gameName] === undefined) {
+    response.sendfile("static/404.html");  
+  } else {
+  response.sendfile("static/game.html");
+  }
+});
+
 app.listen(8889);
 
 
-// Nonsense
 
+// Nonsense
 var users = [];
+var games = [];
 
 // ========================
 // === Socket.io server ===
@@ -35,4 +62,79 @@ io.sockets.on('connection', function (socket) {
     io.sockets.emit('update', data);
   });
 
+  socket.on('requestGame', function(data) {
+    var myGame = games[data.name];
+    if (myGame !== undefined) {
+      if (myGame.joinGame(data.password)) {
+        myGame.addPlayer(data.username, socket);
+        socket.emit("joinGame", {gameName : data.name});  
+      } else {
+        socket.emit("requestGameFailed", 
+        {msg : "The password you supplied does not match."});  
+      }
+    } else {      
+      myGame = new cards.Game(data.username, socket, 
+                            data.privy, data.password, 
+                            data.numPlayers, data.name);
+      console.log("requesting game");
+      if (games[data.name] !== undefined) {
+        socket.emit("requestGameFailed", 
+          {msg : "A game with that name already exists."});
+      } else {
+        games[data.name] = myGame;
+        socket.emit("joinGame", {gameName : data.name});
+      }
+    }
+    // Have to create a Game here. Will work on it next
+  })
+
+
 });
+
+// ======================
+// = ROUTES FOR DB AUTH =
+// ======================
+
+app.get('/', function(req, res){
+            res.sendfile('static/index.html');
+});
+
+app.get('/me', function(req, res){
+    mongoExpressAuth.checkLogin(req, res, function(err){
+        if (err)
+            res.send(err);
+        else {
+            mongoExpressAuth.getAccount(req, function(err, result){
+                if (err)
+                    res.send(err);
+                else 
+                    res.send(result); // NOTE: direct access to the database is a bad idea in a real app
+            });
+        }
+    });
+});
+
+app.post('/login', function(req, res){
+    mongoExpressAuth.login(req, res, function(err){
+        if (err)
+            res.send(err);
+        else
+            res.send('ok');
+    });
+});
+
+app.post('/logout', function(req, res){
+    mongoExpressAuth.logout(req, res);
+    res.send('ok');
+});
+
+app.post('/register', function(req, res){
+    mongoExpressAuth.register(req, function(err){
+        if (err)
+            res.send(err);
+        else
+            res.send('ok');
+    });
+});
+
+app.use(express.static(__dirname + '/static/'));
--- static/main.js
+++ static/main.js
@@ -1,27 +1,49 @@
-var socket;
+window.socket;
+window.username;
 
 $(document).ready(function(){
   socket = io.connect("http://localhost:8888");
-   
-  socket.on('connect', function() {
-    console.log("Connected.");
-  });
-
-  socket.on('news', function(data) {
-      console.log(data);
-  });
 
   socket.on('update', function(data) {
     $("#chat").append($("<li>").html(data.msg));
   });
 
+  socket.on("joinGame", function(data) {
+    var splitLoc = location.href.split("/");
+    var newLoc = splitLoc[0] + "//" + splitLoc[2] + 
+                  "/game/" + data.gameName;
+    window.location.assign(newLoc);
+  });
+
+  socket.on("requestGameFailed", function(data) {
+    alert(data.msg);
+  });
+
   $("#post").click(doSend);
   $("#private").change(handleGamePW);
 
+  window.LoginManager.setLoginSuccess(hideLogin);
+      
 });
 
+function hideLogin(){
+  if ($(".loginBar").hasClass("hidden")) {
+    $(".loginBar").removeClass("hidden");
+  } else {
+    $(".loginBar").addClass("hidden");
+  }
+
+  if ($(".loginFields").hasClass("hidden")) {
+    $(".loginFields").removeClass("hidden");
+  } else {
+    $(".loginFields").addClass("hidden");
+  }
+
+  username = $("#loginButton").val();
+
+}
+
 function handleGamePW() {  
-  console.log("hey");
   if ($("#gamePW").hasClass("hidden")) {
     $("#gamePW").removeClass("hidden");
   } else {
@@ -29,26 +51,44 @@ function handleGamePW() {
   }
 }
 
-function doLogin() {
-  var userName = $('#name').val();
-  socket.emit('login', {"userName": userName});
-}
-
 function doSend() {
   var text = $('#text').val();
   socket.emit('send', { msg : text});
 }
 
-function getForm() {
+function requestGame() {
   var groupName = $("#groupName").val()
   var numPlayers = $("#numPlayers").val()
   var privy = $("#private").is(":checked");
   var password;
 
-  console.log(groupName, numPlayers, privy);
   if (privy) {
     password = $("#gamePW").val();
-    console.log(password);
+    console.log("Private with password" + password);
   }
 
+  socket.emit("requestGame", 
+    { 
+      name: groupName,
+      numPlayers: numPlayers,
+      private: privy,
+      password: password,
+      username: username
+    }
+  );
+}
+
+function checkLogin() {
+  var cookies = document.cookie.split('; ')
+  for (var i = cookies.length - 1; i >= 0; i--) {
+    if (cookies[i].indexOf("username=") === 0) {
+      var cookie = cookies[i];
+      username = cookie.substring("username=".length, cookie.length);
+    }
+    if (cookies[i].indexOf("password=") === 0) {
+      var cookie = cookies[i];
+      password = cookie.substring("password=".length, cookie.length);
+    }
+
+  };
 }
